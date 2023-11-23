6da25a3ae602a61780c08befc2df058599a370ae
Jumaana-bit <71558928+Jumaana-bit@users.noreply.github.com>
2023-11-22 23:51:06 -0500

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
- const bookingURL = new URL("insert.php", window.location.href);
-    bookingURL.searchParams.append('fname', fname);
-    bookingURL.searchParams.append('name', name);
-    bookingURL.searchParams.append('dob', dob);
-    bookingURL.searchParams.append('email', email);
-    bookingURL.searchParams.append('phone_num', phone_num);
-    bookingURL.searchParams.append('baggage', baggage);
-    bookingURL.searchParams.append('cargo', cargo);
